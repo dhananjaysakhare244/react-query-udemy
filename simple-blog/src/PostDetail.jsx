3bf37cc892967b535c8d29b054dbc045c4580653
dsagm7 <dsagm7>
2024-09-05 13:37:29 +0530

    initial checkin

---
+++ simple-blog/src/PostDetail.jsx
@@ -0,0 +1,69 @@
+import { useQuery, useMutation } from "react-query";
+async function fetchComments(postId) {
+  const response = await fetch(
+    `https://jsonplaceholder.typicode.com/comments?postId=${postId}`
+  );
+  return response.json();
+}
+
+async function deletePost(postId) {
+  const response = await fetch(
+    `https://jsonplaceholder.typicode.com/postId/${postId}`,
+    { method: "DELETE" }
+  );
+  return response.json();
+}
+
+async function updatePost(postId) {
+  const response = await fetch(
+    `https://jsonplaceholder.typicode.com/postId/${postId}`,
+    { method: "PATCH", data: { title: "REACT QUERY FOREVER!!!!" } }
+  );
+  return response.json();
+}
+
+export function PostDetail({ post }) {
+  // replace with useQuery
+  const { data, isError, isLoading } = useQuery(
+    ["fetchComments", post.id],
+    () => fetchComments(post.id)
+  );
+  const deleteMutation = useMutation((postId) => deletePost(postId));
+  const updateMutation = useMutation((postId) => updatePost(postId));
+  if (isLoading) return <h3>Loading...</h3>;
+  if (isError) return <h3>Oops, Something went wrong</h3>;
+  return (
+    <>
+      <h3 style={{ color: "blue" }}>{post.title}</h3>
+      <button onClick={() => deleteMutation.mutate(post.id)}>Delete</button>
+      {deleteMutation.isError && (
+        <p style={{ color: "red" }}>Error deleting post!</p>
+      )}
+      {deleteMutation.isLoading && (
+        <p style={{ color: "orange" }}>Deleting in progress</p>
+      )}
+      {deleteMutation.isSuccess && (
+        <p style={{ color: "green" }}>Post has (not) been deleted</p>
+      )}
+      <button onClick={() => updateMutation.mutate(post.id)}>
+        Update title
+      </button>
+      {updateMutation.isError && (
+        <p style={{ color: "red" }}>Error updating post title!</p>
+      )}
+      {updateMutation.isLoading && (
+        <p style={{ color: "orange" }}>Updating in progress</p>
+      )}
+      {updateMutation.isSuccess && (
+        <p style={{ color: "green" }}>Post title has (not) been updated</p>
+      )}
+      <p>{post.body}</p>
+      <h4>Comments</h4>
+      {data.map((comment) => (
+        <li key={comment.id}>
+          {comment.email}: {comment.body}
+        </li>
+      ))}
+    </>
+  );
+}
