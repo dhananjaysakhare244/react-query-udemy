7382f65916f0c66f487aae3b42c582f95fa1b22a
dsagm7 <dsagm7>
2024-09-06 16:35:29 +0530

    added react queryin app  added query for treatments added centralize laoder

--- spa-app/client/src/components/app/App.tsx
+++ spa-app/client/src/components/app/App.tsx
@@ -1,6 +1,9 @@
 import { ChakraProvider } from "@chakra-ui/react";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
+import { queryClient } from "../../react-query/queryClient";
 import { Home } from "./Home";
 import { Loading } from "./Loading";
 import { Navbar } from "./Navbar";
@@ -17,21 +20,24 @@ import { theme } from "@/theme";
 export function App() {
   return (
     <ChakraProvider theme={theme}>
-      <AuthContextProvider>
-        <Loading />
-        <BrowserRouter>
-          <Navbar />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/Staff" element={<AllStaff />} />
-            <Route path="/Calendar" element={<Calendar />} />
-            <Route path="/Treatments" element={<Treatments />} />
-            <Route path="/signin" element={<Signin />} />
-            <Route path="/user/:id" element={<UserProfile />} />
-          </Routes>
-        </BrowserRouter>
-        <ToastContainer />
-      </AuthContextProvider>
+      <QueryClientProvider client={queryClient}>
+        <AuthContextProvider>
+          <Loading />
+          <BrowserRouter>
+            <Navbar />
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/Staff" element={<AllStaff />} />
+              <Route path="/Calendar" element={<Calendar />} />
+              <Route path="/Treatments" element={<Treatments />} />
+              <Route path="/signin" element={<Signin />} />
+              <Route path="/user/:id" element={<UserProfile />} />
+            </Routes>
+          </BrowserRouter>
+          <ToastContainer />
+        </AuthContextProvider>
+        <ReactQueryDevtools />
+      </QueryClientProvider>
     </ChakraProvider>
   );
 }
