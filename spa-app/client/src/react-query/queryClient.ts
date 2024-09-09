5b48749ed3b77114922df23e7d0285f46ebd86bc
dsagm7 <dsagm7>
2024-09-09 20:24:21 +0530

    added error handling for mutation and wrote first mutation

--- spa-app/client/src/react-query/queryClient.ts
+++ spa-app/client/src/react-query/queryClient.ts
@@ -1,15 +1,17 @@
-import { QueryCache, QueryClient } from "@tanstack/react-query";
+import { MutationCache, QueryCache, QueryClient } from "@tanstack/react-query";
 
 import { toast } from "../components/app/toast";
 
-function errorHandler(errorMsg: string) {
+function createTitle(errorMsg: string, actionType: "query" | "mutation") {
+  const action = actionType === "query" ? "fetch" : "update";
+  return `could not ${action} data: ${
+    errorMsg ?? "error connecting to server"
+  }`;
+}
+function errorHandler(title: string) {
   const id = "react-query-toast";
 
   if (!toast.isActive(id)) {
-    const action = "fetch";
-    const title = `could not ${action} data: ${
-      errorMsg ?? "error connecting to server"
-    }`;
     toast({ id, title, status: "error", variant: "subtle", isClosable: true });
   }
 }
@@ -24,7 +26,14 @@ export const queryClient = new QueryClient({
   },
   queryCache: new QueryCache({
     onError: (error) => {
-      errorHandler(error.message);
+      const title = createTitle(error.message, "query");
+      errorHandler(title);
+    },
+  }),
+  mutationCache: new MutationCache({
+    onError: (error) => {
+      const title = createTitle(error.message, "mutation");
+      errorHandler(title);
     },
   }),
 });
