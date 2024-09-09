ea7c0034a8f4b6f41cb4e7a5200aed78af672f9c
dsagm7 <dsagm7>
2024-09-09 15:48:11 +0530

    update and remove queries

--- spa-app/client/src/components/user/hooks/useUser.ts
+++ spa-app/client/src/components/user/hooks/useUser.ts
@@ -1,8 +1,9 @@
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
 import { AxiosResponse } from "axios";
 
 import type { User } from "@shared/types";
 
+import { queryKeys } from "../../../react-query/constants";
 import { generateUserKey } from "../../../react-query/key-factories";
 
 import { useLoginData } from "@/auth/AuthContext";
@@ -22,6 +23,7 @@ async function getUser(userId: number, userToken: string) {
 
 export function useUser() {
   const { userId, userToken } = useLoginData();
+  const queryClient = useQueryClient();
   const { data: user } = useQuery({
     enabled: !!userId,
     queryKey: generateUserKey(userId, userToken),
@@ -29,9 +31,16 @@ export function useUser() {
     staleTime: Infinity,
   });
 
-  function updateUser(newUser: User): void {}
+  function updateUser(newUser: User): void {
+    queryClient.setQueryData(
+      generateUserKey(newUser.id, newUser.token),
+      newUser
+    );
+  }
 
-  function clearUser() {}
+  function clearUser() {
+    queryClient.removeQueries({ queryKey: [queryKeys.user] });
+  }
 
   return { user, updateUser, clearUser };
 }
