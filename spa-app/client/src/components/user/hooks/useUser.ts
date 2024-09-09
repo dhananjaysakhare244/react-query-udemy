3e90d15feb78a830855f667374e7f9170ca46d4b
dsagm7 <dsagm7>
2024-09-09 15:40:37 +0530

    add userdata query and enabled option

--- spa-app/client/src/components/user/hooks/useUser.ts
+++ spa-app/client/src/components/user/hooks/useUser.ts
@@ -1,36 +1,37 @@
+import { useQuery } from "@tanstack/react-query";
 import { AxiosResponse } from "axios";
 
 import type { User } from "@shared/types";
 
+import { generateUserKey } from "../../../react-query/key-factories";
+
 import { useLoginData } from "@/auth/AuthContext";
 import { axiosInstance, getJWTHeader } from "@/axiosInstance";
-import { queryKeys } from "@/react-query/constants";
 
 // query function
-// async function getUser(userId: number, userToken: string) {
-//   const { data }: AxiosResponse<{ user: User }> = await axiosInstance.get(
-//     `/user/${userId}`,
-//     {
-//       headers: getJWTHeader(userToken),
-//     }
-//   );
+async function getUser(userId: number, userToken: string) {
+  const { data }: AxiosResponse<{ user: User }> = await axiosInstance.get(
+    `/user/${userId}`,
+    {
+      headers: getJWTHeader(userToken),
+    }
+  );
 
-//   return data.user;
-// }
+  return data.user;
+}
 
 export function useUser() {
-  // TODO: call useQuery to update user data from server
-  const user: User = null;
+  const { userId, userToken } = useLoginData();
+  const { data: user } = useQuery({
+    enabled: !!userId,
+    queryKey: generateUserKey(userId, userToken),
+    queryFn: () => getUser(userId, userToken),
+    staleTime: Infinity,
+  });
 
-  // meant to be called from useAuth
-  function updateUser(newUser: User): void {
-    // TODO: update the user in the query cache
-  }
+  function updateUser(newUser: User): void {}
 
-  // meant to be called from useAuth
-  function clearUser() {
-    // TODO: reset user to null in query cache
-  }
+  function clearUser() {}
 
   return { user, updateUser, clearUser };
 }
