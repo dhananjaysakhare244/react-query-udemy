adf551717ec223d9cc93d65038f3d27d548a1eff
dsagm7 <dsagm7>
2024-09-10 16:45:19 +0530

    tests for appointment and custom hooks

--- spa-app/client/src/test-utils/index.tsx
+++ spa-app/client/src/test-utils/index.tsx
@@ -1,19 +1,19 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { render as RtlRender } from "@testing-library/react";
-import { ReactElement } from "react";
+import { PropsWithChildren, ReactElement } from "react";
 import { MemoryRouter } from "react-router-dom";
 
 import { queryClientOptions } from "../react-query/queryClient";
 
 // ** FOR TESTING CUSTOM HOOKS ** //
 // from https://tkdodo.eu/blog/testing-react-query#for-custom-hooks
-// export const createQueryClientWrapper = () => {
-//   const queryClient = generateQueryClient();
-//   return ({ children }: PropsWithChildren) => (
-//     <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
-//   );
-// };
+export const createQueryClientWrapper = () => {
+  const queryClient = generateQueryClient();
+  return ({ children }: PropsWithChildren) => (
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+  );
+};
 
 const generateQueryClient = () => {
   queryClientOptions.defaultOptions.queries.retry = false;
