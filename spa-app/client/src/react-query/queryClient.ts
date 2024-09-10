e458579041839e135de4482f13f409191ef2c700
dsagm7 <dsagm7>
2024-09-10 16:17:12 +0530

    testing queries with error

--- spa-app/client/src/react-query/queryClient.ts
+++ spa-app/client/src/react-query/queryClient.ts
@@ -1,4 +1,9 @@
-import { MutationCache, QueryCache, QueryClient } from "@tanstack/react-query";
+import {
+  MutationCache,
+  QueryCache,
+  QueryClient,
+  QueryClientConfig,
+} from "@tanstack/react-query";
 
 import { toast } from "../components/app/toast";
 
@@ -16,7 +21,7 @@ function errorHandler(title: string) {
   }
 }
 
-export const queryClient = new QueryClient({
+export const queryClientOptions: QueryClientConfig = {
   defaultOptions: {
     queries: {
       staleTime: 600000,
@@ -36,4 +41,6 @@ export const queryClient = new QueryClient({
       errorHandler(title);
     },
   }),
-});
+};
+
+export const queryClient = new QueryClient(queryClientOptions);
